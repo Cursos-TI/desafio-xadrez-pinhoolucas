17ecb526c53d736d7afd6eeb61e15b45b98fc14c
Pinho <pinhopaeslucas@gmail.com>
2025-12-22 16:42:36 +0000

    Desafio Novato Tema 4

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,35 @@
+/*O Objetivo aqui é imprimir a trajetória das peças
+sem uma lógica profunda no código
+
+Torre: 5 x Direita
+Bispo: 5x Cima, Direita 
+Rainha: 8x Esquerda
+*/
+
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int main(){
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int bispo=1, rainha=1;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    //Movimentação da Torre 5x Direita
+    for(int i=1; i<=5; i++){
+        printf("\nTorre -> Direita!");
+    }
+    
+    printf("\n");
+    //Movimentação do Bispo 5x Cima, Direita
+    do{
+        printf("\nBispo -> Cima, Direita!");
+        bispo++;
+    } while(bispo<=5);
+    
+    printf("\n");
+    //Movimentação da Rainha 8x Esquerda
+    while(rainha<=8){
+        printf("\nRainha -> Esquerda!");
+        rainha++;
+    }
 
     return 0;
 }
