5a57c3c39a9cd71bbef1a4670fb0a5da3dc248c7
Pinho <pinhopaeslucas@gmail.com>
2025-12-29 07:42:55 -0300

    identacao

--- xadrez.c
+++ xadrez.c
@@ -22,23 +22,26 @@ int main(){
     //Movimentação da Torre 5x Direita
     for(int i=1; i<=5; i++){
         printf("\nTorre -> Direita!");
-    }
-    
+    }    
     printf("\n");
+
+
     //Movimentação do Bispo 5x Cima, Direita
     do{
         printf("\nBispo -> Cima, Direita!");
         bispo++;
-    } while(bispo<=5);
-    
+    } while(bispo<=5);    
     printf("\n");
+
+
     //Movimentação da Rainha 8x Esquerda
     while(rainha<=8){
         printf("\nRainha -> Esquerda!");
         rainha++;
     }
-
     printf("\n");
+
+    
     //Movimentação do Cavalo 2x Baixo + 1 Esquerda
     for(cavalo=1; cavalo<=2; cavalo++){
         
